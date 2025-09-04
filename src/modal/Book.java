3bb5ae9338801601e9efb503803271203a2d4064
sameergupta12281998 <sameergupta122898@gmail.com>
2025-09-04 23:52:01 +0530

    create book modal class

---
+++ src/modal/Book.java
@@ -0,0 +1,27 @@
+package modal;
+
+public class Book {
+    private final String title;
+    private final String author;
+    private final String isbn;
+    private final int publicationYear;
+
+    public Book(String title, String author, String isbn, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.publicationYear = publicationYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", publicationYear=" + publicationYear +
+                '}';
+    }
+
+
+}
